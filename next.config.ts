4daad07b7e48c1805f591d735f27eb07b5e0c42a
minhmarcom <nhatminh.marcom@gmail.com>
2026-08-15 10:37:28 +0700

    Enforce canonical non-www domain https://mangcaubaden.vn and redirect www to non-www via 301 permanent redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.mangcaubaden.vn",
+          },
+        ],
+        destination: "https://mangcaubaden.vn/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
